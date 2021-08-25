350cb79c266f5b1bd306b17cda8079462fc15c9b
holy frege <holy_frege@fastmail.com>
2021-08-25 14:06:53 +0900

    show table in watermeter

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   #root 'application#hello'
   root to: "welcome#index"
-  get '/watermeter/index', to: 'watermeters#index'
+  get '/watermeter', to: 'water_meters#index'
 end
