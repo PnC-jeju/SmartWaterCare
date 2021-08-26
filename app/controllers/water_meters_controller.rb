06ad2216b5031ed006429dd306fde65a4b342ef1
holy frege <holy_frege@fastmail.com>
2021-08-26 14:12:43 +0900

    changed 30 water meter table rows

--- app/controllers/water_meters_controller.rb
+++ app/controllers/water_meters_controller.rb
@@ -1,6 +1,6 @@
 class WaterMetersController < ApplicationController
   def index
-    @pagy, @watermeters = pagy(WaterMeter.order(id: :desc).limit(500), item: 10)
+    @pagy, @watermeters = pagy(WaterMeter.order(id: :desc).limit(500), item: 40)
   end
   def list
 
