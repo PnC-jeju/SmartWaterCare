14dd7ee0b2385084170c909a9ad83e16047f5c50
holy frege <holy_frege@fastmail.com>
2021-08-26 15:14:36 +0900

    changed pagy configuration into initializers

--- app/controllers/water_meters_controller.rb
+++ app/controllers/water_meters_controller.rb
@@ -1,6 +1,6 @@
 class WaterMetersController < ApplicationController
   def index
-    @pagy, @watermeters = pagy(WaterMeter.order(id: :desc).limit(500), items: 20, cycle: true )
+    @pagy, @watermeters = pagy(WaterMeter.order(id: :desc).limit(500))
   end
   def list
 
--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,1 +1,3 @@
 require 'pagy/extras/bootstrap'
+Pagy::VARS[:items] = 25
+Pagy::VARS[:count] = 10
