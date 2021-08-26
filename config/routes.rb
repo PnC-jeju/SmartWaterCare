6d04df852e5ada188cd1f33fc32e46a371563025
holy frege <holy_frege@fastmail.com>
2021-08-26 10:19:42 +0900

    changed routing table

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   #root 'application#hello'
   root to: "welcome#index"
   get '/watermeter', to: 'water_meters#index'
-  get '/pressure_sensor', to: 'pressure_sensors#index'
+  get '/pressure', to: 'pressure_sensors#index'
 end
