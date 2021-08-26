049ff9d405d3a2db88e93cb8a1d7e2ea7a7272b4
holy frege <holy_frege@fastmail.com>
2021-08-26 14:37:44 +0900

    changed pagy counts

--- app/controllers/water_meters_controller.rb
+++ app/controllers/water_meters_controller.rb
@@ -1,6 +1,6 @@
 class WaterMetersController < ApplicationController
   def index
-    @pagy, @watermeters = pagy(WaterMeter.order(id: :desc).limit(500), count: 12 )
+    @pagy, @watermeters = pagy(WaterMeter.order(id: :desc).limit(500), count: 12, items: 10, cycle: true )
   end
   def list
 
