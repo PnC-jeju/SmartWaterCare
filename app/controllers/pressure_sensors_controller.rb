7acb960a3ce37f4769020114e870c28db3c6dc81
holy frege <holy_frege@fastmail.com>
2021-08-26 10:24:07 +0900

    modified errorta name of model in Controller

--- app/controllers/pressure_sensors_controller.rb
+++ app/controllers/pressure_sensors_controller.rb
@@ -1,5 +1,5 @@
 class PressureSensorsController < ApplicationController
   def index
-    @pagy, @pressuresensors= pagy(PressureSensors.order(id: :desc).limit(500), item: 10)
+    @pagy, @pressuresensors= pagy(PressureSensor.order(id: :desc).limit(500), item: 10)
   end
 end
