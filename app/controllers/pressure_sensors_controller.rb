5e6d132b26f37a90e331d381f78368b0193cc096
holy frege <holy_frege@fastmail.com>
2021-09-09 16:33:04 +0900

    cannot allowed login page without login

--- app/controllers/pressure_sensors_controller.rb
+++ app/controllers/pressure_sensors_controller.rb
@@ -1,4 +1,5 @@
 class PressureSensorsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @pagy, @ps= pagy(PressureSensor.order(id: :desc).limit(500), item: 10)
   end
--- app/controllers/water_meters_controller.rb
+++ app/controllers/water_meters_controller.rb
@@ -1,4 +1,5 @@
 class WaterMetersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @pagy, @watermeters = pagy(WaterMeter.order(id: :desc).limit(500))
   end
