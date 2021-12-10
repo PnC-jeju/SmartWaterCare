7a975011c6af109e7d27606095a83ee376d01d65
holy frege <pncltd@hanmail.net>
2021-12-10 23:10:44 +0900

    test fetch

--- app/frontend/packs/c3test.js
+++ app/frontend/packs/c3test.js
@@ -1,63 +1,100 @@
 const dayjs = require('dayjs');
 const timeNow = () => dayjs().format('YYYY-MM-DD HH:mm:ss');
-console.log(timeNow());
+const timeTail = () => dayjs().subtract(1, 'm').format('YYYY-MM-DD HH:mm:ss');
 
-function fetchdata(){
- $.ajax({
-  url: '/api/v1/realtimedata',
-   type: 'get',
-   
-   success: function(response){
-     var value = [];
-     for (var index in response) {
-       console.log(response[index]);
-       value.push(response[index]);
-     }
-     // Perform operation on the return value
-     
-     var chart = c3.generate({
-       bindto: "#flowrate_linechart",
-       data: {
-	 json: value,
-	 keys: {
-	   /* value: ['flowrate', 'totalvolume'] */
-	   value: ['flowrate'] 
-	 },
-       },
-       axis: {
-	 x: {
-	   tick: {
-	     culling: {
-	       max:20
-	     }
-	   }
-	 }
-       }
-     });
+const chartAxis = {
+  x: {
+    type: 'timeseries',
+    min: timeTail(),
+    max: timeNow(),
+    tick: {
+      fit: false,
+      rotate: -30,
+      format: '%Y-%m-%d %H:%M:%S',
+    }
+  }
+}
 
-     var chart = c3.generate({
-       bindto: "#totalvolume_linechart",
-       data: {
-	 json: value,
-	 keys: {
-	   /* value: ['flowrate', 'totalvolume'] */
-	   value: ['totalvolume'] 
-	 },
-       },
-       axis: {
-	 x: {
-	   tick: {
-	     culling: {
-	       max:20
-	     }
-	   }
-	 }
-       }
-     });
-     
-}})}
+const chartData = {
+  x: 'x',
+  xFormat: '%Y-%m-%d %H:%M:%S',
+  columns: [
+    ['x',],
+    ['data1',]
+  ]
+}
+
+let chart = c3.generate({
+    bindto: '#chart',
+    data: chartData,
+    axis: chartAxis,
+});
+
+
+
+
+/* 
+ * function fetchdata(){
+ *  $.ajax({
+ *   url: '/api/v1/realtimedata',
+ *    type: 'get',
+ *    
+ *    success: function(response){
+ *      var value = [];
+ *      for (var index in response) {
+ *        console.log(response[index]);
+ *        value.push(response[index]);
+ *      }
+ *      var chart = c3.generate({
+ *        bindto: "#flowrate_linechart",
+ *        data: {
+ * 	 json: value,
+ * 	 keys: {
+ * 	   value: ['flowrate'] 
+ * 	 },
+ *        },
+ *        axis: {
+ * 	 x: {
+ * 	   tick: {
+ * 	     culling: {
+ * 	       max:20
+ * 	     }
+ * 	   }
+ * 	 }
+ *        }
+ *      });
+ * 
+ *      var chart = c3.generate({
+ *        bindto: "#totalvolume_linechart",
+ *        data: {
+ * 	 json: value,
+ * 	 keys: {
+ * 	   value: ['totalvolume'] 
+ * 	 },
+ *        },
+ *        axis: {
+ * 	 x: {
+ * 	   tick: {
+ * 	     culling: {
+ * 	       max:20
+ * 	     }
+ * 	   }
+ * 	 }
+ *        }
+ *      });
+ *      
+ * }})} */
 
 $(document).ready(function(){
-  setInterval(fetchdata,2000);
+  /* setInterval(fetchdata,2000); */
   /* fetchdata(); */
+  setInterval(() => {
+  // redraw time series axis in every second
+    chart.axis.min({x: timeTail()});
+    chart.axis.max({x: timeNow()});
+
+    chartData.columns[0].push(timeNow());
+    chartData.columns[1].push(Math.random());
+    chart.load({columns: chartData.columns});
+  }, 1000)  
 });
