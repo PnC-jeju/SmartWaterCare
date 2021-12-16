18902d559da3b5fce6c289bb3727cb8a70647689
holy frege <pncltd@hanmail.net>
2021-12-16 14:53:09 +0900

    changed speed

--- app/frontend/packs/c3test.js
+++ app/frontend/packs/c3test.js
@@ -1,6 +1,10 @@
 const dayjs = require('dayjs');
 const timeNow = () => dayjs().format('YYYY-MM-DD HH:mm:ss');
 const timeTail = () => dayjs().subtract(1, 'm').format('YYYY-MM-DD HH:mm:ss');
+const flowchart_period=800;
+const totalvolumechart_period=7000;
+const pressurechart_period=2000;
+const temperaturechart_period=7000;
 
 /* I just wanted to use class or reuse sth, but it's not easy and I've no time to be done so just done it as follows */
 /* flowchart */
@@ -25,7 +29,67 @@ let temperatureChartData = { x: 'x', xFormat: '%Y-%m-%d %H:%M:%S', columns: [ ['
 let temperatureChart = c3.generate({ bindto: '#temperature_linechart', data: temperatureChartData, axis: temperatureChartAxis,});
 
 
-function realtimeInterval()
+/* function realtimeInterval()
+ * {
+ *     setInterval(() => {
+ *     $.ajax({
+ *       url: '/api/v1/realtimedata',
+ *       type: 'post',
+ *       data: {
+ * 	chosen_dong: "flowrate",
+ * 	chosen_roomtype: "flowrate",	
+ * 	chosen_datatype: "flowrate"
+ *       },
+ *       error: function (error) {
+ *  	console.log(error);
+ *       },
+ *       success: function(response){
+ *  	flowratevalue = response['flowrate'];
+ *  	totalvolumevalue = response['totalvolume'];
+ *  	temperaturevalue = response['temperature'];
+ *  	pressurevalue = response['pressure']; */
+
+	/* flowchart */
+ 	/* flowrateChart.axis.min({x: timeTail()});
+ 	   flowrateChart.axis.max({x: timeNow()});
+	   
+ 	   flowrateChartData.columns[0].push(timeNow());
+ 	   flowrateChartData.columns[1].push(flowratevalue);
+ 	   flowrateChart.load({columns: flowrateChartData.columns}); */
+
+	/* totalvolume */
+/* totalvolumeChart.axis.min({x: timeTail()});
+   totalvolumeChart.axis.max({x: timeNow()});
+   
+   totalvolumeChartData.columns[0].push(timeNow());
+   totalvolumeChartData.columns[1].push(totalvolumevalue);
+   totalvolumeChart.load({columns: totalvolumeChartData.columns}); */
+
+	/* pressurechart */
+/* pressureChart.axis.min({x: timeTail()});
+   pressureChart.axis.max({x: timeNow()});
+   
+   pressureChartData.columns[0].push(timeNow());
+   pressureChartData.columns[1].push(pressurevalue);
+   pressureChart.load({columns: pressureChartData.columns});
+ */
+
+	/* temperaturechart */
+/* temperatureChart.axis.min({x: timeTail()});
+   temperatureChart.axis.max({x: timeNow()});
+   
+   temperatureChartData.columns[0].push(timeNow());
+   temperatureChartData.columns[1].push(temperaturevalue);
+   temperatureChart.load({columns: temperatureChartData.columns});
+   },
+   complete: function() {
+   console.log("end");
+   }
+   });
+   }, 1000);  
+   } */
+
+function realFlowrateInterval()
 {
     setInterval(() => {
     $.ajax({
@@ -41,39 +105,104 @@ function realtimeInterval()
       },
       success: function(response){
  	flowratevalue = response['flowrate'];
- 	totalvolumevalue = response['totalvolume'];
- 	temperaturevalue = response['temperature'];
- 	pressurevalue = response['pressure'];
-
 	/* flowchart */
- 	console.log(response);
-	
  	flowrateChart.axis.min({x: timeTail()});
  	flowrateChart.axis.max({x: timeNow()});
 	
  	flowrateChartData.columns[0].push(timeNow());
  	flowrateChartData.columns[1].push(flowratevalue);
  	flowrateChart.load({columns: flowrateChartData.columns});
+      },
+      complete: function() {
+ 	console.log("end");
+      }
+    });
+  }, flowchart_period);  
+}
 
-	/* totalvolume */
-	totalvolumeChart.axis.min({x: timeTail()});
- 	totalvolumeChart.axis.max({x: timeNow()});
-	
- 	totalvolumeChartData.columns[0].push(timeNow());
- 	totalvolumeChartData.columns[1].push(totalvolumevalue);
- 	totalvolumeChart.load({columns: totalvolumeChartData.columns});
 
-	/* pressurechart */
-	pressureChart.axis.min({x: timeTail()});
+function realPressureInterval()
+{
+    setInterval(() => {
+    $.ajax({
+      url: '/api/v1/realtimedata',
+      type: 'post',
+      data: {
+	chosen_dong: "flowrate",
+	chosen_roomtype: "flowrate",	
+	chosen_datatype: "flowrate"
+      },
+      error: function (error) {
+ 	console.log(error);
+      },
+      success: function(response){
+ 	pressurevalue = response['flowrate'];
+	/* flowchart */
+ 	pressureChart.axis.min({x: timeTail()});
  	pressureChart.axis.max({x: timeNow()});
 	
  	pressureChartData.columns[0].push(timeNow());
  	pressureChartData.columns[1].push(pressurevalue);
  	pressureChart.load({columns: pressureChartData.columns});
-	
+      },
+      complete: function() {
+ 	console.log("end");
+      }
+    });
+  }, pressurechart_period);  
+}
 
-	/* temperaturechart */
-	temperatureChart.axis.min({x: timeTail()});
+
+function realTotalvolumeInterval()
+{
+    setInterval(() => {
+    $.ajax({
+      url: '/api/v1/realtimedata',
+      type: 'post',
+      data: {
+	chosen_dong: "flowrate",
+	chosen_roomtype: "flowrate",	
+	chosen_datatype: "flowrate"
+      },
+      error: function (error) {
+ 	console.log(error);
+      },
+      success: function(response){
+ 	totalvolumevalue = response['totalvolume'];
+	/* flowchart */
+ 	totalvolumeChart.axis.min({x: timeTail()});
+ 	totalvolumeChart.axis.max({x: timeNow()});
+	
+ 	totalvolumeChartData.columns[0].push(timeNow());
+ 	totalvolumeChartData.columns[1].push(totalvolumevalue);
+ 	totalvolumeChart.load({columns: totalvolumeChartData.columns});
+      },
+      complete: function() {
+ 	console.log("end");
+      }
+    });
+  }, totalvolumechart_period);  
+}
+
+
+function realTemperatureInterval()
+{
+    setInterval(() => {
+    $.ajax({
+      url: '/api/v1/realtimedata',
+      type: 'post',
+      data: {
+	chosen_dong: "flowrate",
+	chosen_roomtype: "flowrate",	
+	chosen_datatype: "flowrate"
+      },
+      error: function (error) {
+ 	console.log(error);
+      },
+      success: function(response){
+ 	temperaturevalue = response['temperature'];
+	/* flowchart */
+ 	temperatureChart.axis.min({x: timeTail()});
  	temperatureChart.axis.max({x: timeNow()});
 	
  	temperatureChartData.columns[0].push(timeNow());
@@ -84,11 +213,15 @@ function realtimeInterval()
  	console.log("end");
       }
     });
-  }, 1000);  
+  }, temperaturechart_period);  
 }
 
 $(document).ready(function(){
-  realtimeInterval();
+  realFlowrateInterval();
+  realTotalvolumeInterval();
+  realPressureInterval();
+  realTemperatureInterval();
+  /* realtimeInterval(); */
 });  
 
  
